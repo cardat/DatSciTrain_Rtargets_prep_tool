5bdfa7a2909d555714d475ca58f381c6ccddd43c
ivanhanigan <ivan.hanigan@gmail.com>
2023-09-14 13:01:23 +0800

    add tool

---
+++ _targets.R
@@ -0,0 +1,14 @@
+library(targets)
+
+lapply(list.files("R", full.names = TRUE), source)
+
+tar_option_set(
+  packages =
+    c("targets",
+      "data.table"
+    )
+)
+
+list(
+#### INSERT YOUR RTARGETS_PREP_TOOL RESULT HERE ####
+)

2493f68694f425a0da2a5ceeed3f901c743e458e
Lucas Hertzog <125840911+lucashertzog@users.noreply.github.com>
2023-11-07 12:19:40 +0800

    Update run.R

--- run.R
+++ run.R
@@ -4,3 +4,8 @@ load_packages(pkg = c("data.table"), do_it = T, force_install = F)
 tar_visnetwork(targets_only = T)
 # If using the tool then you only have a skeleton.  You need to write code then:
 # tar_make()
+
+# useful
+tar_invalidate()
+tar_objects()
+tar_load_everything()

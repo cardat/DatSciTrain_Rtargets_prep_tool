0e8b5d19bcc365b49959180cfe908b41b8a47800
Ivan Hanigan <ivan.hanigan@gmail.com>
2023-11-07 12:18:20 +0800

    Update run.R

--- run.R
+++ run.R
@@ -1,7 +1,6 @@
 library(targets)
 tar_source()
 load_packages(pkg = c("data.table"), do_it = T, force_install = F)
-lapply(list.files("R", full.names = TRUE), source)
 tar_visnetwork(targets_only = T)
 # If using the tool then you only have a skeleton.  You need to write code then:
 # tar_make()

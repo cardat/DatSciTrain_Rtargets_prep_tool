8f5e1c16f0efa8224ee3347356d77b411237da52
Ivan Hanigan <ivan.hanigan@gmail.com>
2023-11-08 11:15:54 +0800

    Update run.R

--- run.R
+++ run.R
@@ -2,6 +2,9 @@ library(targets)
 tar_source()
 load_packages(pkg = c("data.table"), do_it = T, force_install = F)
 tar_visnetwork(targets_only = T)
+# for very dense pipelines use this layout
+# library(visNetwork)
+# visIgraphLayout(tar_visnetwork(targets_only = T), layout = 'layout.kamada.kawai', physics = T)
 # If using the tool then you only have a skeleton.  You need to write code then:
 # tar_make()
 

a4323631a71e40710e6c0782be639214c4d3d934
ivanhanigan <ivan.hanigan@gmail.com>
2024-11-15 08:07:48 +0800

    add some helpful tips

--- run.R
+++ run.R
@@ -1,14 +1,17 @@
 library(targets)
 tar_source()
-load_packages(pkg = c("data.table"), do_it = T, force_install = F)
+load_packages(pkg = c("targets"), do_it = T, force_install = F)
+# this is to help running in interactive mode
+# make sure you also add packages to the _targets.R file as well 
 tar_visnetwork(targets_only = T)
 # for very dense pipelines use this layout
 # library(visNetwork)
 # visIgraphLayout(tar_visnetwork(targets_only = T), layout = 'layout.kamada.kawai', physics = T)
 # If using the tool then you only have a skeleton.  You need to write code then:
-# tar_make()
+tar_make()
 
-# useful
-tar_invalidate()
-tar_objects()
-tar_load_everything()
+# # useful tools
+# tar_meta(fields = error, complete_only = TRUE)
+# tar_invalidate()
+# tar_objects()
+# tar_load_everything()
